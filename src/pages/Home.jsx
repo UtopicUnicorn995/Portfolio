7753a0dff0767ee6fe426bcb6e500edddd4563b3
utopicUnicorn <christianbarrios995@gmail.com>
2022-11-03 20:54:48 +0800

    added padding to the h1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ function Home() {
   return (
     <div className="homepage ">
       <div className="container  justify-content-center align-items-center ">
-        <h1 className="homeH1 mb-3">
+        <h1 className="homeH1 mb-3 mt-2">
           Ian is a <span className="color-primary">Full-Stack developer</span>{" "}
           and
           <span className="color-primary"> React Native developer</span>
         </h1>
 
         <p className="secondary-color">
-          He crafts responsive websites and user friendly mobile devices where
-          technologies meet creativity
+          He crafts responsive websites and user friendly mobile apps where
+          technologies meet creativity.
         </p>
         <div className="mb-3 img-container  justify-content-center align-items-center">
           <img className="img-fluid" src={personImg} alt="" />
