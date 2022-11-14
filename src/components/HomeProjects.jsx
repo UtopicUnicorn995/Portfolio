495ea6520e8e27a0a85591ecbcdab701b5e6152f
utopicUnicorn <christianbarrios995@gmail.com>
2022-11-14 11:27:43 +0800

    added a fourth project

--- src/components/HomeProjects.jsx
+++ src/components/HomeProjects.jsx
@@ -5,7 +5,7 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 // import HouseMarketImg from "../assets/HouseMarketImg.png";
-import { FaArrowRight, FaArrowsAltH, FaDesktop } from "react-icons/fa";
+import { FaArrowRight, FaArrowsAltH, FaCode } from "react-icons/fa";
 
 function HomeProjects() {
   return (
@@ -42,7 +42,7 @@ function HomeProjects() {
         <Row>
           <Col
             xs={12}
-            lg={4}
+            lg={3}
             className="d-flex justify-content-center align-items-center"
           >
             <Card
@@ -78,7 +78,7 @@ function HomeProjects() {
                   <Col className="justify-content-center d-flex">
                     <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
                       Demo&nbsp;
-                      <FaDesktop />
+                      <FaCode />
                     </Link>
                   </Col>
                 </Row>
@@ -86,7 +86,7 @@ function HomeProjects() {
             </Card>
           </Col>
           <Col
-            lg={4}
+            lg={3}
             xs={12}
             className="d-flex justify-content-center align-items-center"
           >
@@ -108,9 +108,9 @@ function HomeProjects() {
                 </Card.Header>
                 <Card.Title>LeChef Culinary School</Card.Title>
                 <Card.Text className="text-secondary">
-                  This is my first project as a web developer. A simple
-                  full-stack web app for LeChef Culinary School where you can
-                  send an email of your enrollment to the Culinary School.
+                  This is my first project as a freelance web developer. A
+                  simple full-stack web app for LeChef Culinary School where you
+                  can send an email of your enrollment to the Culinary School.
                 </Card.Text>
                 <Row>
                   <Col className="justify-content-center d-flex">
@@ -122,7 +122,7 @@ function HomeProjects() {
                   <Col className="justify-content-center d-flex">
                     <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
                       Demo&nbsp;
-                      <FaDesktop />
+                      <FaCode />
                     </Link>
                   </Col>
                 </Row>
@@ -130,7 +130,7 @@ function HomeProjects() {
             </Card>
           </Col>
           <Col
-            lg={4}
+            lg={3}
             xs={12}
             className="d-flex justify-content-center align-items-center"
           >
@@ -166,7 +166,51 @@ function HomeProjects() {
                   <Col className="justify-content-center d-flex">
                     <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
                       Demo&nbsp;
-                      <FaDesktop />
+                      <FaCode />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col
+            lg={3}
+            xs={12}
+            className="d-flex justify-content-center align-items-center"
+          >
+            <Card
+              body
+              className="m-2 rounded-0 border border-secondary"
+              style={{ width: "18rem" }}
+            >
+              <Card.Img
+                className="rounded-0"
+                variant="top"
+                src={require("../assets/yt.png")}
+              />
+              <Card.Body>
+                <Card.Header className="spanHead">
+                  <span className="card-span">ReactJS</span>{" "}
+                  <span className="card-span">Material UI</span>{" "}
+                  <span className="card-span">RAPID API</span>{" "}
+                </Card.Header>
+                <Card.Title>Youtube Clone</Card.Title>
+                <Card.Text className="text-secondary">
+                  This web app is made from a code along tutorial in youtube
+                  where you get to learn fetch API, utilizing the utils, using
+                  material UI for the CSS and building a functional react app.
+                </Card.Text>
+                <Row>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Live&nbsp;
+                      <FaArrowsAltH />
+                    </Link>
+                  </Col>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Demo&nbsp;
+                      <FaCode />
                     </Link>
                   </Col>
                 </Row>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -21,7 +21,7 @@ function Projects() {
         <Row>
           <Col
             xs={12}
-            lg={4}
+            lg={3}
             className="d-flex justify-content-center align-items-center"
           >
             <Card
@@ -56,7 +56,7 @@ function Projects() {
                   </Col>
                   <Col className="justify-content-center d-flex">
                     <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
+                      Demo&nbsp;
                       <FaCode />
                     </Link>
                   </Col>
@@ -65,7 +65,7 @@ function Projects() {
             </Card>
           </Col>
           <Col
-            lg={4}
+            lg={3}
             xs={12}
             className="d-flex justify-content-center align-items-center"
           >
@@ -87,9 +87,9 @@ function Projects() {
                 </Card.Header>
                 <Card.Title>LeChef Culinary School</Card.Title>
                 <Card.Text className="text-secondary">
-                  This is my first project as a web developer. A simple
-                  full-stack web app for LeChef Culinary School where you can
-                  send an email of your enrollment to the Culinary School.
+                  This is my first project as a freelance web developer. A
+                  simple full-stack web app for LeChef Culinary School where you
+                  can send an email of your enrollment to the Culinary School.
                 </Card.Text>
                 <Row>
                   <Col className="justify-content-center d-flex">
@@ -100,7 +100,7 @@ function Projects() {
                   </Col>
                   <Col className="justify-content-center d-flex">
                     <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
+                      Demo&nbsp;
                       <FaCode />
                     </Link>
                   </Col>
@@ -109,7 +109,7 @@ function Projects() {
             </Card>
           </Col>
           <Col
-            lg={4}
+            lg={3}
             xs={12}
             className="d-flex justify-content-center align-items-center"
           >
@@ -144,54 +144,7 @@ function Projects() {
                   </Col>
                   <Col className="justify-content-center d-flex">
                     <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
-                      <FaCode />
-                    </Link>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="my-5">
-        <h4 className="text-hover text-white">
-          <span className="primary-color">#</span>small projects
-        </h4>
-      </Container>
-      <Container>
-        <Row>
-          <Col
-            lg={3}
-            xs={12}
-            className="d-flex justify-content-center align-items-center mb-lg-4"
-          >
-            <Card
-              body
-              className="m-2 rounded-0 border border-secondary"
-              style={{ width: "18rem" }}
-            >
-              <Card.Img
-                className="rounded-0"
-                variant="top"
-                src={require("../assets/css.gif")}
-              />
-              <Card.Body>
-                <Card.Title>CSS only Bouncing animations</Card.Title>
-                <Card.Text className="text-secondary">
-                  This is a simple css only animation with a 3D effect that
-                  looks good as a screensaver.
-                </Card.Text>
-                <Row>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      Live&nbsp;
-                      <FaArrowsAltH />
-                    </Link>
-                  </Col>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
+                      Demo&nbsp;
                       <FaCode />
                     </Link>
                   </Col>
@@ -202,7 +155,7 @@ function Projects() {
           <Col
             lg={3}
             xs={12}
-            className="d-flex justify-content-center align-items-center mb-lg-4"
+            className="d-flex justify-content-center align-items-center"
           >
             <Card
               body
@@ -212,13 +165,19 @@ function Projects() {
               <Card.Img
                 className="rounded-0"
                 variant="top"
-                src={require("../assets/dadJoke.gif")}
+                src={require("../assets/yt.png")}
               />
               <Card.Body>
-                <Card.Title>Dad Jokes API</Card.Title>
+                <Card.Header className="spanHead">
+                  <span className="card-span">ReactJS</span>{" "}
+                  <span className="card-span">Material UI</span>{" "}
+                  <span className="card-span">RAPID API</span>{" "}
+                </Card.Header>
+                <Card.Title>Youtube Clone</Card.Title>
                 <Card.Text className="text-secondary">
-                  This web app is mainly made using CSS and JS to fetch corny
-                  dad jokes from an API.
+                  This web app is made from a code along tutorial in youtube
+                  where you get to learn fetch API, utilizing the utils, using
+                  material UI for the CSS and building a functional react app.
                 </Card.Text>
                 <Row>
                   <Col className="justify-content-center d-flex">
@@ -229,159 +188,7 @@ function Projects() {
                   </Col>
                   <Col className="justify-content-center d-flex">
                     <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
-                      <FaCode />
-                    </Link>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col
-            lg={3}
-            xs={12}
-            className="d-flex justify-content-center align-items-center mb-lg-4"
-          >
-            <Card
-              body
-              className="m-2 rounded-0 border border-secondary"
-              style={{ width: "18rem" }}
-            >
-              <Card.Img
-                className="rounded-0"
-                variant="top"
-                src={require("../assets/consoleHomePage.gif")}
-              />
-              <Card.Body>
-                <Card.Title>Comparing consoles homepage</Card.Title>
-                <Card.Text className="text-secondary">
-                  A simple home page of an web app that I plan to make in the
-                  future.
-                </Card.Text>
-                <Row>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      Live&nbsp;
-                      <FaArrowsAltH />
-                    </Link>
-                  </Col>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
-                      <FaCode />
-                    </Link>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col
-            lg={3}
-            xs={12}
-            className="d-flex justify-content-center align-items-center mb-lg-4"
-          >
-            <Card
-              body
-              className="m-2 rounded-0 border border-secondary"
-              style={{ width: "18rem" }}
-            >
-              <Card.Img
-                className="rounded-0"
-                variant="top"
-                src={require("../assets/scrolling.gif")}
-              />
-              <Card.Body>
-                <Card.Title>To-do-list scroll animation</Card.Title>
-                <Card.Text className="text-secondary">
-                  A cool scrolling animation using CSS and JS that I found in
-                  udemy that I plan to use someday.
-                </Card.Text>
-                <Row>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      Live&nbsp;
-                      <FaArrowsAltH />
-                    </Link>
-                  </Col>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
-                      <FaCode />
-                    </Link>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col
-            lg={3}
-            xs={12}
-            className="d-flex justify-content-center align-items-center mb-lg-4"
-          >
-            <Card
-              body
-              className="m-2 rounded-0 border border-secondary"
-              style={{ width: "18rem" }}
-            >
-              <Card.Img
-                className="rounded-0"
-                variant="top"
-                src={require("../assets/page.gif")}
-              />
-              <Card.Body>
-                <Card.Title>Rotating Navigation</Card.Title>
-                <Card.Text className="text-secondary">
-                  A rotating navigation using JS and CSS that I could use in my
-                  next portfolio.
-                </Card.Text>
-                <Row>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      Live&nbsp;
-                      <FaArrowsAltH />
-                    </Link>
-                  </Col>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
-                      <FaCode />
-                    </Link>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col
-            lg={3}
-            xs={12}
-            className="d-flex justify-content-center align-items-center mb-lg-4"
-          >
-            <Card
-              body
-              className="m-2 rounded-0 border border-secondary"
-              style={{ width: "18rem" }}
-            >
-              <Card.Img
-                className="rounded-0"
-                variant="top"
-                src={require("../assets/accordion.gif")}
-              />
-              <Card.Body>
-                <Card.Title>Customized accordion display</Card.Title>
-                <Card.Text className="text-secondary">
-                  A simple customized accordion display that I plan to use. This
-                  is made by manipulating the JS and CSS on button click.
-                </Card.Text>
-                <Row>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      Live&nbsp;
-                      <FaArrowsAltH />
-                    </Link>
-                  </Col>
-                  <Col className="justify-content-center d-flex">
-                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
-                      code&nbsp;
+                      Demo&nbsp;
                       <FaCode />
                     </Link>
                   </Col>
