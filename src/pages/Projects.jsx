da9e72f933c57a19077415175a2c215ff12aec14
utopicUnicorn <christianbarrios995@gmail.com>
2022-11-14 11:33:22 +0800

    reverted some of the codes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -198,6 +198,243 @@ function Projects() {
           </Col>
         </Row>
       </Container>
+      <Container className="mt-5 mb-3">
+        <h4 className="text-hover text-white">
+          <span className="primary-color">#</span>small projects
+        </h4>
+      </Container>
+      <Container>
+        <Row>
+          <Col
+            lg={3}
+            xs={12}
+            className="d-flex justify-content-center align-items-center mb-lg-4"
+          >
+            <Card
+              body
+              className="m-2 rounded-0 border border-secondary"
+              style={{ width: "18rem" }}
+            >
+              <Card.Img
+                className="rounded-0"
+                variant="top"
+                src={require("../assets/css.gif")}
+              />
+              <Card.Body>
+                <Card.Title>CSS only Bouncing animations</Card.Title>
+                <Card.Text className="text-secondary">
+                  This is a simple css only animation with a 3D effect that
+                  looks good as a screensaver.
+                </Card.Text>
+                <Row>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Live&nbsp;
+                      <FaArrowsAltH />
+                    </Link>
+                  </Col>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      code&nbsp;
+                      <FaCode />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col
+            lg={3}
+            xs={12}
+            className="d-flex justify-content-center align-items-center mb-lg-4"
+          >
+            <Card
+              body
+              className="m-2 rounded-0 border border-secondary"
+              style={{ width: "18rem" }}
+            >
+              <Card.Img
+                className="rounded-0"
+                variant="top"
+                src={require("../assets/dadJoke.gif")}
+              />
+              <Card.Body>
+                <Card.Title>Dad Jokes API</Card.Title>
+                <Card.Text className="text-secondary">
+                  This web app is mainly made using CSS and JS to fetch corny
+                  dad jokes from an API.
+                </Card.Text>
+                <Row>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Live&nbsp;
+                      <FaArrowsAltH />
+                    </Link>
+                  </Col>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      code&nbsp;
+                      <FaCode />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col
+            lg={3}
+            xs={12}
+            className="d-flex justify-content-center align-items-center mb-lg-4"
+          >
+            <Card
+              body
+              className="m-2 rounded-0 border border-secondary"
+              style={{ width: "18rem" }}
+            >
+              <Card.Img
+                className="rounded-0"
+                variant="top"
+                src={require("../assets/consoleHomePage.gif")}
+              />
+              <Card.Body>
+                <Card.Title>Comparing consoles homepage</Card.Title>
+                <Card.Text className="text-secondary">
+                  A simple home page of an web app that I plan to make in the
+                  future.
+                </Card.Text>
+                <Row>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Live&nbsp;
+                      <FaArrowsAltH />
+                    </Link>
+                  </Col>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      code&nbsp;
+                      <FaCode />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col
+            lg={3}
+            xs={12}
+            className="d-flex justify-content-center align-items-center mb-lg-4"
+          >
+            <Card
+              body
+              className="m-2 rounded-0 border border-secondary"
+              style={{ width: "18rem" }}
+            >
+              <Card.Img
+                className="rounded-0"
+                variant="top"
+                src={require("../assets/scrolling.gif")}
+              />
+              <Card.Body>
+                <Card.Title>To-do-list scroll animation</Card.Title>
+                <Card.Text className="text-secondary">
+                  A cool scrolling animation using CSS and JS that I found in
+                  udemy that I plan to use someday.
+                </Card.Text>
+                <Row>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Live&nbsp;
+                      <FaArrowsAltH />
+                    </Link>
+                  </Col>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      code&nbsp;
+                      <FaCode />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col
+            lg={3}
+            xs={12}
+            className="d-flex justify-content-center align-items-center mb-lg-4"
+          >
+            <Card
+              body
+              className="m-2 rounded-0 border border-secondary"
+              style={{ width: "18rem" }}
+            >
+              <Card.Img
+                className="rounded-0"
+                variant="top"
+                src={require("../assets/page.gif")}
+              />
+              <Card.Body>
+                <Card.Title>Rotating Navigation</Card.Title>
+                <Card.Text className="text-secondary">
+                  A rotating navigation using JS and CSS that I could use in my
+                  next portfolio.
+                </Card.Text>
+                <Row>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Live&nbsp;
+                      <FaArrowsAltH />
+                    </Link>
+                  </Col>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      code&nbsp;
+                      <FaCode />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col
+            lg={3}
+            xs={12}
+            className="d-flex justify-content-center align-items-center mb-lg-4"
+          >
+            <Card
+              body
+              className="m-2 rounded-0 border border-secondary"
+              style={{ width: "18rem" }}
+            >
+              <Card.Img
+                className="rounded-0"
+                variant="top"
+                src={require("../assets/accordion.gif")}
+              />
+              <Card.Body>
+                <Card.Title>Customized accordion display</Card.Title>
+                <Card.Text className="text-secondary">
+                  A simple customized accordion display that I plan to use. This
+                  is made by manipulating the JS and CSS on button click.
+                </Card.Text>
+                <Row>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      Live&nbsp;
+                      <FaArrowsAltH />
+                    </Link>
+                  </Col>
+                  <Col className="justify-content-center d-flex">
+                    <Link className="btn btn-text text-secondary border-secondary rounded-0 card-button">
+                      code&nbsp;
+                      <FaCode />
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
